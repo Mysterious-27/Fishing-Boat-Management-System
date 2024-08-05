f46b9eb8506cf195883c7b8388ae9f71691a523b
P V ADHITHYA <121473249+Mysterious-27@users.noreply.github.com>
2024-08-05 21:28:18 +0530

    Update Conn.java

--- src/fishing/boat/management/system/Conn.java
+++ src/fishing/boat/management/system/Conn.java
@@ -22,7 +22,7 @@ public class Conn {
     public Conn() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/fms","root","123456karunE#");
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/(name of your database) ","(username)","(password)");
             s = c.createStatement();
         } catch (Exception e) {
             e.printStackTrace();
